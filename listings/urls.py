43b4c90030a7acc04aef39ac4ccd95dc3aa26e98
Simamkele44 <simamkele44@gmail.com>
2021-07-19 07:41:51 +0200

    first

---
+++ listings/urls.py
@@ -0,0 +1,9 @@
+from .models import Listing
+from django.urls import path
+from .views import ListingsView, ListingView, SearchView
+
+urlpatterns = [
+    path('', ListingsView.as_view()),
+    path('search', SearchView.as_view()),
+    path('<slug>', ListingView.as_view()),
+]
